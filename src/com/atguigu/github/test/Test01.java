2f847511af8134210b18c8ca2103ef0b0f0c5fe0
shangchenghui <m13503841395@163.com>
2016-09-18 21:24:42 +0800

    new

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -7,6 +7,7 @@ public class Test01 {
 		System.out.println("Hello Github 服务器");
 		System.out.println("Hello 服务器回应");
 		System.out.println("update m1 insert");
+		System.out.println("update m1 insert");
+		
 	}
-
 }
