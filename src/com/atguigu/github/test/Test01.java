9ceb37e5a08d0866632f201a383d7c3261c15a26
shangchenghui <m13503841395@163.com>
2016-09-18 21:00:31 +0800

    1
    1

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -5,7 +5,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("Hello0427");
 		System.out.println("Hello Github 服务器");
-
+		System.out.println("Hello 服务器回应");
 	}
 
 }
